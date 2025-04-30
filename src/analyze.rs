c62ec8694a5df1b870f4d6b25e9b80e6fe2fbcfb
Kamil Stawiarski <33175431+ora600pl@users.noreply.github.com>
2025-04-30 19:13:11 +0200

    Refactoring - separete module for macros

--- src/analyze.rs
+++ src/analyze.rs
@@ -24,6 +24,8 @@ use regex::*;
 use crate::Args;
 use crate::reasonings::{chat_gpt, gemini};
 
+use crate::make_notes;
+
 struct TopStats {
     events: BTreeMap<String, u8>,
     bgevents: BTreeMap<String, u8>,
@@ -31,34 +33,7 @@ struct TopStats {
     stat_names: BTreeMap<String, u8>,
 }
 
-#[macro_export]
-macro_rules! make_notes {
-    ($file:expr, $quiet:expr, $($arg:tt)*) => {{
-        use std::io::Write;
-        // if $quiet is false we will write output to screen
-        if !$quiet {
-            print!($($arg)*);
-        }
 
-        let formatted = format!($($arg)*);
-
-        //Create plain text from colored one
-        let plain = {
-            // remove everything starting with ESC ( \x1B ), than [, digits and ;,
-            // and ending with 'm' or 'K'.
-            let re = regex::Regex::new(r"\x1B\[[0-9;]*[mK]").unwrap();
-            re.replace_all(&formatted, "").to_string()
-        };
-
-        // Let's append to a logfile
-        let mut file = std::fs::OpenOptions::new()
-            .append(true)
-            .create(true)
-            .open($file)
-            .expect("Can't open file");
-        write!(file, "{}", plain).expect("Unable to write to log file");
-    }};
-}
 
 //We don't want to plot everything, because it would cause to much trouble 
 //we need to find only essential wait events and SQLIDs 
